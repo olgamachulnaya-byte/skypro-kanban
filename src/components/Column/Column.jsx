b0c5f8fe3a8fa4dedb6208d391ba47bdc2ef7031
Olga <olga.machulnaya@gmail.com>
2026-04-17 11:07:08 +0300

    5 Commit

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,6 +1,12 @@
 import Card from '../Card/Card'
 
 function Column({ title, cards }) {
+  const topicClasses = {
+    'Web Design': '_orange',
+    Research: '_green',
+    Copywriting: '_purple',
+  }
+
   return (
     <div className="main__column column">
       <div className="column__title">
@@ -8,11 +14,11 @@ function Column({ title, cards }) {
       </div>
 
       <div className="cards">
-        {cards.map((card, index) => (
+        {cards.map((card) => (
           <Card
-            key={`${title}-${index}`}
-            themeClass={card.themeClass}
-            themeText={card.themeText}
+            key={card.id}
+            themeClass={topicClasses[card.topic]}
+            themeText={card.topic}
             title={card.title}
             date={card.date}
           />
