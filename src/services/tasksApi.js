9cfe70588f29afbc5319d068bc0f40b89f23d113
Olga <olga.machulnaya@gmail.com>
2026-05-04 20:10:19 +0300

    17 Commit

--- src/services/tasksApi.js
+++ src/services/tasksApi.js
@@ -1,3 +1,7 @@
 import { apiRequest } from './apiClient'
 
 export const getTasks = () => apiRequest('/tasks', { auth: true })
+export const getTaskById = (id) => apiRequest(`/tasks/${id}`, { auth: true })
+export const createTask = (payload) => apiRequest('/tasks', { method: 'POST', body: payload, auth: true })
+export const updateTask = (id, payload) => apiRequest(`/tasks/${id}`, { method: 'PUT', body: payload, auth: true })
+export const deleteTask = (id) => apiRequest(`/tasks/${id}`, { method: 'DELETE', auth: true })
